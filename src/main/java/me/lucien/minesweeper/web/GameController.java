f2d6ddefc5818b36945676939b3d805e09d85bfb
syYing <706580680@qq.com>
2020-01-31 20:57:38 +0800

    Use @RequestParam instead of @RequestBody and add room capacity

--- src/main/java/me/lucien/minesweeper/web/GameController.java
+++ src/main/java/me/lucien/minesweeper/web/GameController.java
@@ -4,8 +4,6 @@ import me.lucien.minesweeper.domain.Room;
 import me.lucien.minesweeper.domain.SquareData;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.boot.json.JsonParser;
-import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,17 +16,15 @@ import java.util.Map;
 public class GameController {
 
     private Map<Integer, Room> roomMap = new HashMap<>();
+    private int capacity = 1000;
 
     @PostMapping("/room")
-    public String createRoom(@RequestBody String input) throws HttpException {
-        if (input == null) {
-            throw new HttpException(HttpStatus.BAD_REQUEST, "Invalid Params");
+    public String createRoom(@RequestParam("width") int width, @RequestParam("height") int height) throws HttpException {
+        if (roomMap.size() >= capacity) {
+            throw  new HttpException(HttpStatus.SERVICE_UNAVAILABLE, "No available room.");
         }
 
-        JsonParser jsonParser = JsonParserFactory.getJsonParser();
-        Map<String, Object> map = jsonParser.parseMap(input);
-
-        Room room = new Room((int) map.get("width"), (int) map.get("height"));
+        Room room = new Room(width, height);
         roomMap.put(room.getRoomId(), room);
 
         JSONObject o = new JSONObject();
@@ -44,7 +40,7 @@ public class GameController {
 
     @GetMapping("/room/{roomId}")
     public List<SquareData> enterRoom(@PathVariable("roomId") int roomId,
-                                      @RequestBody String roomKey) throws HttpException {
+                                      @RequestParam("roomKey") String roomKey) throws HttpException {
         checkLegitimacy(roomId, roomKey);
 
         Room room = roomMap.get(roomId);
@@ -52,16 +48,16 @@ public class GameController {
         return room.getGameData();
     }
 
-    @PostMapping("/room/{roomId}")
+    @DeleteMapping("/room/{roomId}")
     public void deleteRoom(@PathVariable("roomId") int roomId,
-                           @RequestBody String roomKey) throws HttpException {
+                           @RequestParam("roomKey") String roomKey) throws HttpException {
         checkLegitimacy(roomId, roomKey);
         roomMap.remove(roomId);
     }
 
     @PostMapping("/room/{roomId}/square/{x}/{y}")
     public List<SquareData> leftClick(@PathVariable("roomId") int roomId, @PathVariable("x") int x, @PathVariable("y") int y,
-                                      @RequestBody String roomKey) throws HttpException {
+                                      @RequestParam("roomKey") String roomKey) throws HttpException {
         checkLegitimacy(roomId, roomKey);
         Room room = roomMap.get(roomId);
 
@@ -76,7 +72,7 @@ public class GameController {
 
     @PatchMapping("/room/{roomId}/square/{x}/{y}")
     public void rightClick(@PathVariable("roomId") int roomId, @PathVariable("x") int x, @PathVariable("y") int y,
-                           @RequestBody String roomKey) throws HttpException {
+                           @RequestParam("roomKey") String roomKey) throws HttpException {
         checkLegitimacy(roomId, roomKey);
         Room room = roomMap.get(roomId);
 
