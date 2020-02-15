1d76f69d41a04ca8c8837e191194b80e6d963d81
syYing <706580680@qq.com>
2020-02-15 21:35:22 +0800

    Optimize code structure

--- src/main/java/me/lucien/minesweeper/domain/RoomManager.java
+++ src/main/java/me/lucien/minesweeper/domain/RoomManager.java
@@ -1,13 +1,9 @@
 package me.lucien.minesweeper.domain;
 
-import me.lucien.minesweeper.web.HttpException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -27,87 +23,48 @@ public class RoomManager {
     private Map<Integer, ScheduledFuture<?>> taskMap = new HashMap<>();
     private ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
-    public String createRoom(int width, int height) throws HttpException {
+    public Room createRoom(int width, int height) {
         if (roomMap.size() >= capacity) {
-            throw new HttpException(HttpStatus.SERVICE_UNAVAILABLE, "No available room.");
+            return null;
         }
 
         Room room = new Room(width, height);
         roomMap.put(room.getId(), room);
 
         ScheduledFuture<?> future = executorService.schedule(
-                () -> { roomMap.remove(room.getId()); }, delay, TimeUnit.SECONDS);
+                () -> roomMap.remove(room.getId()),
+                delay, TimeUnit.SECONDS);
         taskMap.put(room.getId(), future);
 
-        JSONObject o = new JSONObject();
-        o.put("id", room.getId());
-        o.put("key", room.getKey());
-        o.put("mineNum", room.getMineNum());
-
-        return o.toString();
+        return room;
     }
 
-    public List<SquareData> enterRoom(int id, String key) throws HttpException {
-        checkLegitimacy(id, key);
-        resetSchedule(id);
-
-        Room room = roomMap.get(id);
-
-        return room.getGameData();
-    }
-
-    public void deleteRoom(int id, String key) throws HttpException {
-        checkLegitimacy(id, key);
-        roomMap.remove(id);
-    }
-
-    public List<SquareData> leftClick(int id, String key, int x, int y) throws HttpException {
-        checkLegitimacy(id, key);
-        resetSchedule(id);
-
-        Room room = roomMap.get(id);
-        if (x < 0 || x >= room.getWidth() || y < 0 || y >= room.getHeight()) {
-            throw new IndexOutOfBoundsException();
-        }
-
-        List<SquareData> res = room.uncover(x, y);
-
-        return res;
-    }
-
-    public int rightClick(int id, String key, int x, int y) throws HttpException {
-        checkLegitimacy(id, key);
-        resetSchedule(id);
-
-        Room room = roomMap.get(id);
-        if (x < 0 || x >= room.getWidth() || y < 0 || y >= room.getHeight()) {
-            throw new IndexOutOfBoundsException();
-        }
-
-        room.flag(x, y);
-
-        return room.getBoard()[x][y].getState().ordinal();
-    }
-
-    private void checkLegitimacy(int id, String key) throws HttpException {
+    public Room getRoom(int id) {
         if (!roomMap.containsKey(id)) {
-            throw new HttpException(HttpStatus.NOT_FOUND, "The room does't exist.");
-        }
+            return null;
+        } else {
+            resetSchedule(id);
 
-        Room room = roomMap.get(id);
-
-        if (!room.getKey().equals(key)) {
-            throw new HttpException(HttpStatus.FORBIDDEN, "The key is wrong.");
+            return roomMap.get(id);
         }
     }
 
-    private void resetSchedule(int id) {
+    public void deleteRoom(int id) {
+        if (roomMap.containsKey(id)) {
+            roomMap.remove(id);
+            taskMap.remove(id);
+        }
+    }
+
+    public void resetSchedule(int id) {
         ScheduledFuture<?> future = taskMap.get(id);
         while (!future.isCancelled()) {
             future.cancel(true);
         }
 
-        future = executorService.schedule(() -> { roomMap.remove(id); }, delay, TimeUnit.SECONDS);
+        future = executorService.schedule(
+                () -> roomMap.remove(id),
+                delay, TimeUnit.SECONDS);
         taskMap.put(id, future);
     }
 }
--- src/main/java/me/lucien/minesweeper/web/GameController.java
+++ src/main/java/me/lucien/minesweeper/web/GameController.java
@@ -1,8 +1,11 @@
 package me.lucien.minesweeper.web;
 
+import me.lucien.minesweeper.domain.Room;
 import me.lucien.minesweeper.domain.RoomManager;
 import me.lucien.minesweeper.domain.SquareData;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,35 +19,78 @@ public class GameController {
 
     @PostMapping("/room")
     public String createRoom(@RequestParam("width") int width, @RequestParam("height") int height) throws HttpException {
-        return roomManager.createRoom(width, height);
+        Room room = roomManager.createRoom(width, height);
+        if (room == null) {
+            throw new HttpException(HttpStatus.SERVICE_UNAVAILABLE, "No available room.");
+        }
+
+        JSONObject o = new JSONObject();
+        o.put("id", room.getId());
+        o.put("key", room.getKey());
+        o.put("mineNum", room.getMineNum());
+
+        return o.toString();
     }
 
     @GetMapping("/room/{id}")
     public List<SquareData> enterRoom(@PathVariable("id") int id,
                                       @RequestParam("key") String key) throws HttpException {
-        return roomManager.enterRoom(id, key);
+        checkLegitimacy(id, key);
+
+        Room room = roomManager.getRoom(id);
+
+        return room.getGameData();
     }
 
     @DeleteMapping("/room/{id}")
     public void deleteRoom(@PathVariable("id") int id,
                            @RequestParam("key") String key) throws HttpException {
-        roomManager.deleteRoom(id, key);
+        checkLegitimacy(id, key);
+
+        roomManager.deleteRoom(id);
     }
 
     @PostMapping("/room/{id}/square/{x}/{y}")
     public List<SquareData> leftClick(@PathVariable("id") int id, @PathVariable("x") int x, @PathVariable("y") int y,
                                       @RequestParam("key") String key) throws HttpException {
-        return roomManager.leftClick(id, key, x, y);
+        checkLegitimacy(id, key);
+
+        Room room = roomManager.getRoom(id);
+        if (x < 0 || x >= room.getWidth() || y < 0 || y >= room.getHeight()) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return room.uncover(x, y);
     }
 
     @PatchMapping("/room/{id}/square/{x}/{y}")
     public int rightClick(@PathVariable("id") int id, @PathVariable("x") int x, @PathVariable("y") int y,
                           @RequestParam("key") String key) throws HttpException {
-        return roomManager.rightClick(id, key, x, y);
+        checkLegitimacy(id, key);
+
+        Room room = roomManager.getRoom(id);
+        if (x < 0 || x >= room.getWidth() || y < 0 || y >= room.getHeight()) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        room.flag(x, y);
+
+        return room.getBoard()[x][y].getState().ordinal();
     }
 
     @ExceptionHandler(HttpException.class)
     public ResponseEntity<String> handleHttpException(HttpException e) {
         return new ResponseEntity<>(e.getMessage(), e.getStatus());
+    }
+
+    private void checkLegitimacy(int id, String key) throws HttpException {
+        Room room = roomManager.getRoom(id);
+        if (room == null) {
+            throw new HttpException(HttpStatus.NOT_FOUND, "The room does't exist.");
+        }
+
+        if (!room.getKey().equals(key)) {
+            throw new HttpException(HttpStatus.FORBIDDEN, "The key is wrong.");
+        }
     }
 }
